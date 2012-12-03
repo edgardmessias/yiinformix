3203c5908890bdbbc8ead2cd003c631dddd6f043
Edgard Lorraine Messias <edgardmessias@gmail.com>
2012-12-03 21:48:36 -0200

    Added CInformixCommandBuilder class;

---
+++ CInformixCommandBuilder.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * CInformixCommandBuilder class file.
+ *
+ * @author Edgard L. Messias <edgardmessias@gmail.com>
+ * @link https://github.com/edgardmessias/yiinformix
+ */
+
+/**
+ * CInformixCommandBuilder provides basic methods to create query commands for tables for Informix Servers.
+ *
+ * @author Edgard L. Messias <edgardmessias@gmail.com>
+ * @package ext.yiinformix
+ */
+class CInformixCommandBuilder extends CDbCommandBuilder {
+    
+}
