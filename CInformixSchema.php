0d3fbab1fb1f141bac8f7df44b89bc016adc3853
edgardmessias <edgardmessias@gmail.com>
2012-12-04 08:43:59 -0200

    Added column length;

--- CInformixSchema.php
+++ CInformixSchema.php
@@ -123,69 +123,75 @@ class CInformixSchema extends CDbSchema {
     protected function findColumns($table) {
 //        return true;
         $sql = <<<EOD
-SELECT syscolumns. *,
-NOT (coltype>255) AS allownull,
-CASE MOD(coltype, 256)
-  WHEN  0 THEN 'char'
-  WHEN  1 THEN 'smallint'
-  WHEN  2 THEN 'integer'
-  WHEN  3 THEN 'float'
-  WHEN  4 THEN 'smallfloat'
-  WHEN  5 THEN 'decimal'
-  WHEN  6 THEN 'serial'
-  WHEN  7 THEN 'date'
-  WHEN  8 THEN 'money'
-  WHEN  9 THEN 'null'
-  WHEN 10 THEN 'datetime'
-  WHEN 11 THEN 'byte'
-  WHEN 12 THEN 'text'
-  WHEN 13 THEN 'varchar'
-  WHEN 14 THEN 'interval'
-  WHEN 15 THEN 'nchar'
-  WHEN 16 THEN 'nvarchar'
-  WHEN 17 THEN 'int8'
-  WHEN 18 THEN 'serial8'
-  WHEN 19 THEN 'set'
-  WHEN 20 THEN 'multiset'
-  WHEN 21 THEN 'list'
-  WHEN 22 THEN 'Unnamed ROW'
-  WHEN 40 THEN 'Variable-length'
-  WHEN 4118 THEN 'Named ROW'
-  ELSE '???'
-END AS type,
-CASE
-    WHEN mod(coltype,256) in (5,8) THEN trunc(collength/256)||","||mod(collength,256)                
-    WHEN mod(coltype,256) in (10,14) THEN                   
-        CASE trunc(mod(collength,256)/16)                        
-            WHEN  0 THEN "YEAR"                        
-            WHEN  2 THEN "MONTH"                        
-            WHEN  4 THEN "DAY"                        
-            WHEN  6 THEN "HOUR"                        
-            WHEN  8 THEN "MINUTE"                        
-            WHEN 10 THEN "SECOND"                        
-            WHEN 11 THEN "FRACTION(1)"                        
-            WHEN 12 THEN "FRACTION(2)"                        
-            WHEN 13 THEN "FRACTION(3)"                        
-            WHEN 14 THEN "FRACTION(4)"                        
-            WHEN 15 THEN "FRACTION(5)"                     
-        END ||"("||trunc(collength/256)+trunc(mod(collength,256)/16)-mod(collength,16)||") : "||                       
-        CASE mod(collength,16)                        
-            WHEN  0 THEN "YEAR"                        
-            WHEN  2 THEN "MONTH"                        
-            WHEN  4 THEN "DAY"                        
-            WHEN  6 THEN "HOUR"                        
-            WHEN  8 THEN "MINUTE"                        
-            WHEN 10 THEN "SECOND"                        
-            WHEN 11 THEN "FRACTION(1)"                        
-            WHEN 12 THEN "FRACTION(2)"                        
-            WHEN 13 THEN "FRACTION(3)"                        
-            WHEN 14 THEN "FRACTION(4)"                        
-            WHEN 15 THEN "FRACTION(5)"                     
-        END                 
-    ELSE ""||collength          
-END collength
+SELECT syscolumns.colname,
+       syscolumns.colmin,
+       syscolumns.colmax,
+    NOT (coltype>255) AS allownull,
+    CASE MOD(coltype, 256)
+        WHEN  0 THEN 'char'
+        WHEN  1 THEN 'smallint'
+        WHEN  2 THEN 'integer'
+        WHEN  3 THEN 'float'
+        WHEN  4 THEN 'smallfloat'
+        WHEN  5 THEN 'decimal'
+        WHEN  6 THEN 'serial'
+        WHEN  7 THEN 'date'
+        WHEN  8 THEN 'money'
+        WHEN  9 THEN 'null'
+        WHEN 10 THEN 'datetime'
+        WHEN 11 THEN 'byte'
+        WHEN 12 THEN 'text'
+        WHEN 13 THEN 'varchar'
+        WHEN 14 THEN 'interval'
+        WHEN 15 THEN 'nchar'
+        WHEN 16 THEN 'nvarchar'
+        WHEN 17 THEN 'int8'
+        WHEN 18 THEN 'serial8'
+        WHEN 19 THEN 'set'
+        WHEN 20 THEN 'multiset'
+        WHEN 21 THEN 'list'
+        WHEN 22 THEN 'Unnamed ROW'
+        WHEN 40 THEN sysxtdtypes.name
+        WHEN 41 THEN sysxtdtypes.name
+        WHEN 52 THEN 'bigint'
+        WHEN 53 THEN 'bigserial'
+        WHEN 4118 THEN 'Named ROW'
+        ELSE '???'
+    END AS type,
+    CASE
+        WHEN mod(coltype,256) in (5,8) THEN trunc(collength/256)||","||mod(collength,256)                
+        WHEN mod(coltype,256) in (10,14) THEN                   
+            CASE trunc(mod(collength,256)/16)                        
+                WHEN  0 THEN "YEAR"                        
+                WHEN  2 THEN "MONTH"                        
+                WHEN  4 THEN "DAY"                        
+                WHEN  6 THEN "HOUR"                        
+                WHEN  8 THEN "MINUTE"                        
+                WHEN 10 THEN "SECOND"                        
+                WHEN 11 THEN "FRACTION(1)"                        
+                WHEN 12 THEN "FRACTION(2)"                        
+                WHEN 13 THEN "FRACTION(3)"                        
+                WHEN 14 THEN "FRACTION(4)"                        
+                WHEN 15 THEN "FRACTION(5)"                     
+            END ||"("||trunc(collength/256)+trunc(mod(collength,256)/16)-mod(collength,16)||") : "||                       
+            CASE mod(collength,16)                        
+                WHEN  0 THEN "YEAR"                        
+                WHEN  2 THEN "MONTH"                        
+                WHEN  4 THEN "DAY"                        
+                WHEN  6 THEN "HOUR"                        
+                WHEN  8 THEN "MINUTE"                        
+                WHEN 10 THEN "SECOND"                        
+                WHEN 11 THEN "FRACTION(1)"                        
+                WHEN 12 THEN "FRACTION(2)"                        
+                WHEN 13 THEN "FRACTION(3)"                        
+                WHEN 14 THEN "FRACTION(4)"                        
+                WHEN 15 THEN "FRACTION(5)"                     
+            END                 
+        ELSE ""||collength          
+    END collength
 FROM systables 
   INNER JOIN syscolumns ON syscolumns.tabid = systables.tabid
+  LEFT JOIN sysxtdtypes on sysxtdtypes.extended_id = syscolumns.extended_id
 WHERE systables.tabid >= 100
 AND   systables.tabname = :table
 AND   systables.owner = :schema
@@ -219,6 +225,16 @@ EOD;
         $c->isPrimaryKey = false;
         $c->isForeignKey = false;
 
+        if (strpos($column['TYPE'], 'char') !== false || strpos($column['TYPE'], 'text') !== false) {
+            $c->size = $column['COLLENGTH'];
+        } elseif (preg_match('/(real|float|double|decimal)/', $column['TYPE'])) {
+            $length = explode(",", $column['COLLENGTH']);
+            $c->size = $length[0];
+            $c->precision = $length[0];
+            $c->scale = $length[1];
+        }
+
+
         if (stripos($column['TYPE'], 'serial') !== false) {
             $c->autoIncrement = true;
         } else {
@@ -226,7 +242,6 @@ EOD;
         }
 
         $c->init($column['TYPE'], null);
-
         return $c;
     }
 
