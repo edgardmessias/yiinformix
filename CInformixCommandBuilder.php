17a4ab67e3e2990d5e129fe80f369c55215fde80
Edgard Lorraine Messias <edgardmessias@gmail.com>
2012-12-03 22:59:52 -0200

    Added applyLimit in CInformixCommandBuilder class;

--- CInformixCommandBuilder.php
+++ CInformixCommandBuilder.php
@@ -14,5 +14,29 @@
  * @package ext.yiinformix
  */
 class CInformixCommandBuilder extends CDbCommandBuilder {
-    
+
+    /**
+     * 
+     * @param string $sql SQL query string.
+     * @param integer $limit maximum number of rows, 0 to ignore limit.
+     * @param integer $offset row offset, 0 to ignore offset.
+     * @return string SQL with limit and offset.
+     */
+    public function applyLimit($sql, $limit, $offset) {
+        $limit = $limit !== null ? (int) $limit : 0;
+        $offset = $offset !== null ? (int) $offset : 0;
+
+        if ($limit > 0) { //just limit
+            $sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(\s+LIMIT\s+\d+\s*)?/i', "\\1SELECT\\2 LIMIT $limit", $sql);
+        } else {
+            $sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(\s+LIMIT\s+\d+\s*)?/i', "\\1SELECT\\2 ", $sql);
+        }
+        if ($offset > 0) { //just limit
+            $sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(\s+LIMIT\s+\d+\s*)?(\s*SKIP\s+\d+\s*)?/i', "\\1SELECT\\2\\3 SKIP $offset ", $sql);
+        } else {
+            $sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(\s+LIMIT\s+\d+\s*)?(\s*SKIP\s+\d+\s*)?/i', "\\1SELECT\\2\\3 ", $sql);
+        }
+        return $sql;
+    }
+
 }
