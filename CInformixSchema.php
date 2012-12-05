421763d1bc7ac8021205310189e2e3eb091fabc6
edgardmessias <edgardmessias@gmail.com>
2012-12-05 08:46:30 -0200

    Added support resetSequence in CInformixSchema;

--- CInformixSchema.php
+++ CInformixSchema.php
@@ -414,4 +414,25 @@ EOD;
                 . ' MODIFY (' . $this->quoteColumnName($column) . ' ' . $this->getColumnType($type) . ')';
     }
 
+    /**
+     * Resets the sequence value of a table's primary key.
+     * The sequence will be reset such that the primary key of the next new row inserted
+     * will have the specified value or 1.
+     * @param CDbTableSchema $table the table schema whose primary key sequence will be reset
+     * @param mixed $value the value for the primary key of the next new row inserted. If this is not set,
+     * the next new row's primary key will have a value 1.
+     */
+    public function resetSequence($table, $value = null) {
+        if ($table->sequenceName !== null && is_string($table->primaryKey)) {
+            if ($value === null)
+                $value = $this->getDbConnection()->createCommand("SELECT MAX({$table->primaryKey}) FROM {$table->rawName}")->queryScalar() + 1;
+            else
+                $value = (int) $value;
+
+            $serialType = $table->getColumn($table->primaryKey)->dbType;
+
+            $this->getDbConnection()->createCommand("ALTER TABLE {$table->rawName} MODIFY ({$table->primaryKey} $serialType ($value)")->execute();
+        }
+    }
+
 }
